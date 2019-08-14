ff9ede5f0d09a803b878a5710c429143bc9c99f9
lyang <Lyangde163@163.com>
2019-08-14 15:16:22 +0800

    娱悦APP-视频—判断用户是否登录-111

--- src/main/java/com/yuyue/app/api/controller/LoginController.java
+++ src/main/java/com/yuyue/app/api/controller/LoginController.java
@@ -1,6 +1,8 @@
 package com.yuyue.app.api.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.yuyue.app.annotation.CurrentUser;
+import com.yuyue.app.annotation.LoginRequired;
 import com.yuyue.app.api.domain.AppUser;
 import com.yuyue.app.api.domain.AppVersion;
 import com.yuyue.app.api.domain.ReturnResult;
@@ -215,4 +217,14 @@ public class LoginController {
          }
          return ResultJSONUtils.getJSONObjectBean(result);
     }
+
+    public  boolean userAuth(AppUser user){
+        AppUser appUserById = loginService.getAppUserById(user.getId());
+        if (appUserById.equals(user.getPassword())){
+            return Boolean.TRUE;
+        }else {
+            return Boolean.FALSE;
+        }
+    }
+
 }
