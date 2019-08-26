29a88a7d1f7097eb6a387d17632204c136e179ad
weiLaoShi <weilingwei_it@163.com>
2019-08-26 09:52:16 +0800

    娱悦APP-合并个人信息

--- src/main/java/com/yuyue/app/api/controller/LoginController.java
+++ src/main/java/com/yuyue/app/api/controller/LoginController.java
@@ -183,9 +183,9 @@ public class LoginController {
 
          Pattern pattern = Pattern.compile("^((13[0-9])|(14[5,7,9])|(15([0-3]|[5-9]))|(166)|(17[0,1,3,5,6,7,8])|(18[0-9])|(19[8|9]))\\d{8}$");
          try {
-             /*if (!code.equals(redisTemplate.opsForValue().get(phone).toString())){
+             if (!code.equals(redisTemplate.opsForValue().get(phone).toString())){
                  result.setMessage("验证码错误！");
-             }else */if(pattern.matcher(phone).matches() == false || phone.length()!=11){
+             }else if(pattern.matcher(phone).matches() == false || phone.length()!=11){
                  result.setMessage("手机号输入错误！");
              } else {
                  AppUser appUserMsgByPhone = loginService.getAppUserMsgByPhone(phone);
