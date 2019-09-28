b87533fc52554565ddbeadcec80d409f6dc93cc7
weiLaoShi <weilingwei_it@163.com>
2019-09-28 12:29:17 +0800

    修改版本号4

--- src/main/java/com/yuyue/app/AppApplication.java
+++ src/main/java/com/yuyue/app/AppApplication.java
@@ -2,6 +2,8 @@ package com.yuyue.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
