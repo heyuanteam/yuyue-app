4a7d0d271757fc7714e80ffb3e08ee03d47a0daf
lyang <Lyangde163@163.com>
2019-09-28 12:42:42 +0800

    娱悦APP-   文件名

--- src/main/java/com/yuyue/app/AppApplication.java
+++ src/main/java/com/yuyue/app/AppApplication.java
@@ -2,8 +2,6 @@ package com.yuyue.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
