32951e9d3cb2589d1cfeda630f0dbcd372b97909
weiLaoShi <weilingwei_it@163.com>
2019-10-09 13:18:44 +0800

    整合极光

--- src/main/java/com/yuyue/app/api/controller/SendSmsController.java
+++ src/main/java/com/yuyue/app/api/controller/SendSmsController.java
@@ -1,22 +1,16 @@
 package com.yuyue.app.api.controller;
 
 import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.google.common.collect.Maps;
-import com.yuyue.app.api.domain.JPush;
 import com.yuyue.app.api.domain.ReturnResult;
-import com.yuyue.app.api.service.SendSmsService;
-import com.yuyue.app.config.JPushClients;
 import com.yuyue.app.utils.RandomSaltUtil;
 import com.yuyue.app.utils.RedisUtil;
 import com.yuyue.app.utils.ResultJSONUtils;
 import com.yuyue.app.utils.SmsUtil;
-import org.apache.commons.collections.CollectionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,12 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequestMapping(value="/send", produces = "application/json; charset=UTF-8")
-//@Component
 public class SendSmsController extends BaseController{
     private static Logger log = LoggerFactory.getLogger(SendSmsController.class);
 
@@ -37,20 +29,6 @@ public class SendSmsController extends BaseController{
     private SmsUtil smsUtil;
     @Autowired
     private RedisUtil redisUtil;
-    @Autowired
-    private JPushClients jPushClients;
-    @Autowired
-    private SendSmsService sendSmsService;
-
-    //极光推送
-    @Value("${jpush.appKey}")
-    private String appKey;
-
-    @Value("${jpush.masterSecret}")
-    private String masterSecret;
-
-    @Value("${jpush.apnsProduction}")
-    private boolean apnsProduction;
 
     private static final String signName = "娱悦APP";
     private static final String templateCode = "SMS_172100731";
@@ -101,45 +79,6 @@ public class SendSmsController extends BaseController{
             e.printStackTrace();
             result.setMessage("短信发送失败！");
             log.info("短信发送失败！");
-        }
-        return ResultJSONUtils.getJSONObjectBean(result);
-    }
-
-    /**
-     * 极光推送
-     */
-    @RequestMapping("/sendJPush")
-    @ResponseBody
-//    @Async // 异步方法
-    public JSONObject sendJPush(HttpServletRequest request) {
-        log.info("极光推送-------------->>/sendJPush/sendJPush");
-        getParameterMap(request);
-//        用户ID,别名
-//        List<String> aliasList = Arrays.asList("239");
-//        String notificationTitle = "通知内容标题";
-//        String msgTitle = "消息内容标题";
-//        String msgContent = "消息内容";
-        List<JPush> list = sendSmsService.getValid();
-        ReturnResult result =new ReturnResult();
-        if(CollectionUtils.isNotEmpty(list)){
-            for (int i = 0; i < list.size(); i++) {
-                JPush jPush = list.get(i);
-                if(jPush != null){
-                    try {
-                        jPushClients.sendToAll(jPush.getNotificationTitle(), jPush.getMsgTitle(), jPush.getMsgContent(), jPush.getExtras(),
-                                apnsProduction,masterSecret,appKey);
-                        sendSmsService.updateValid("10B",jPush.getId());
-                        result.setMessage("极光推送成功!");
-                        result.setResult(JSONArray.parseArray(JSON.toJSONString(list)));
-                        result.setStatus(Boolean.TRUE);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        log.info("极光推送失败！");
-                        result.setMessage("极光推送失败！");
-                        sendSmsService.updateValid("10C",jPush.getId());
-                    }
-                }
-            }
         }
         return ResultJSONUtils.getJSONObjectBean(result);
     }
