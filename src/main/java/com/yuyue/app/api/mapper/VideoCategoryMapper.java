96ed717abb92ae6f53435eddcfe04ae452299a6b
weiLaoShi <weilingwei_it@163.com>
2019-10-15 10:25:58 +0800

    修改APP菜单字段

--- src/main/java/com/yuyue/app/api/mapper/VideoCategoryMapper.java
+++ src/main/java/com/yuyue/app/api/mapper/VideoCategoryMapper.java
@@ -8,7 +8,8 @@ import java.util.List;
 
 @Repository
 public interface VideoCategoryMapper extends MyBaseMapper<VideoCategory> {
-    @Select("SELECT id,category ,DATE_FORMAT(upload_time ,'%Y-%m-%d %H:%i:%s') uploadTime  ,url,description,category_no categoryNo FROM yuyue_category ORDER BY category_no")
+    @Select("SELECT id,category ,DATE_FORMAT(upload_time ,'%Y-%m-%d %H:%i:%s') uploadTime  ,url,description,category_no categoryNo FROM yuyue_category " +
+            " where `status` = '10B' ORDER BY category_no")
     List<VideoCategory> getVideoCategory();
 
 }
