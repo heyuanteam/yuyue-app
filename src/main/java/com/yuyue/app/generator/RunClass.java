9be3b21d6548d9c1513d16f6bd75a03cfc2b95a2
weiLaoShi <weilingwei_it@163.com>
2019-12-10 15:21:49 +0800

    yuyue-app-swagger

--- src/main/java/com/yuyue/app/generator/RunClass.java
+++ src/main/java/com/yuyue/app/generator/RunClass.java
@@ -1,11 +1,10 @@
-package com.yuyue.app.test;
+package com.yuyue.app.generator;
 
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
 
-import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
